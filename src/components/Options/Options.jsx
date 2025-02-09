d64d416d5c00d17541a978d081296c22012dd3c0
Radyslav Krasnov <radislavkrasnoff@gmail.com>
2025-02-09 23:27:21 +0200

    add styles

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,10 +1,12 @@
+import css from './Options.module.css'
+
 const Options = ({ updateFeedback, totalFeedback, resetFeedback }) => {
   return (
     <>
-      <button onClick={() => updateFeedback('good')}>Good</button>
-      <button onClick={() => updateFeedback('neutral')}>Neutral</button>
-      <button onClick={() => updateFeedback('bad')}>Bad</button>
-      {totalFeedback > 0 && <button onClick={resetFeedback}>Reset</button>}
+      <button className={css.btn} onClick={() => updateFeedback('good')}>Good</button>
+      <button className={css.btn} onClick={() => updateFeedback('neutral')}>Neutral</button>
+      <button className={css.btn} onClick={() => updateFeedback('bad')}>Bad</button>
+      {totalFeedback > 0 && <button className={css.btn} onClick={resetFeedback}>Reset</button>}
     </>
   );
 };
